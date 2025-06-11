9ca5276e271812164c3e3dad03d2ecf316c5787d
bboyison <onurcankyabahis@gmail.com>
2025-06-11 14:13:08 +0300

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'Arial' }}>
+      <h1>Diş Tedavisi Maliyet Hesaplayıcısı</h1>
+      <p>Bu sayfa yakında interaktif form içerecek.</p>
+    </div>
+  );
+}
